f75bf422b5d15e9ba382b5505a4ac091341f7701
yuval feldman <feldmanyuval@gmail.com>
2019-12-11 12:08:29 +0200

    added no attribute check for help command

--- csepy/PublicFunctions/SupportCommands/HelpCommand.py
+++ csepy/PublicFunctions/SupportCommands/HelpCommand.py
@@ -6,13 +6,11 @@ from csepy.System.EndpointMap.EndpointMapper import CommandMapping
 class HelpCommand(ICommand):
     def Execute(self):
         helpText = "\n".join(["\t\t{0:30}{1}".format(command, CommandMapping[command].ShortHelp)
-                              for command in CommandMapping if command != "help"])
+                              for command in CommandMapping if command != "help"
+                              and hasattr(CommandMapping[command], "ShortHelp")])
         helpText = f"Public functions available. for more information type " \
                    f"--help after the desired function\n{helpText}"
         self.context.Logger.INFO(helpText)
 
 
 HelpCommand.PublicFacing = "help"
-HelpCommand.MinRequestParameters = 0
-HelpCommand.Help = "Help"
-HelpCommand.ShortHelp = "help"
